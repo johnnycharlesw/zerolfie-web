9636bad83d697aada729fa2a422d18d04e076126
John Charles Woods <woods.johnny.charles@gmail.com>
2025-09-10 19:05:37 -0400

    Implement automodernization script for legacy web content and add real-time CSS application for deprecated elements

---
+++ default.js
@@ -0,0 +1,32 @@
+(()=>{
+    // Automodernization script: Fixes old web content to use modern standards BTS so old websites still function
+    
+    // Replace blink elements with modern versions
+    document.querySelectorAll('blink').forEach(element => {
+        let modernVersionOfElement=document.createElement('span');
+        modernVersionOfElement.classList.add('-zerolfie-web-internals-blink');
+        modernVersionOfElement.innerHTML=element.innerHTML;
+        element.replaceWith(modernVersionOfElement);
+    });
+    
+})();
+// Apply CSS fpor deprecated elements in real time
+async function cssApplyLoop(){
+    while (true){
+        cssApply();
+        await new Promise(r => setTimeout(r, 1000));  // 1-second delay
+    }
+}
+function cssApply(){
+    setTimeout(blinkElements, 1000);
+}
+
+function blinkElements(){
+    document.querySelectorAll('span.-zerolfie-web-internals-blink').forEach(element => {
+        if (element.style.display==='none'){
+            element.style.display="inherit";
+        }else{
+            element.style.display="none";
+        }
+    })
+}
